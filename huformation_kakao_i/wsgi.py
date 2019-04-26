8265feaf0114d6a746fb8c3331628da6dd00be46
roharon <roharon98@gmail.com>
2019-04-26 23:55:03 +0900

    create : project build
    프로젝트 첫삽

---
+++ huformation_kakao_i/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for huformation_kakao_i project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huformation_kakao_i.settings')
+
+application = get_wsgi_application()
